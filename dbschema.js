5b364d6d7ee23ea0dc37e20ecc146b8df1c20a87
Jerome Aragones <joni.jinon@awsys-i.com>
2020-07-08 15:36:23 +0800

    ReactJS_Training_D03_Add&GetUserProfileDetails

--- dbschema.js
+++ dbschema.js
@@ -1,11 +1,48 @@
 let db = {
+    users: [
+        {
+            userId: `dasdjojh13123123`,
+            email: `user@email.com`,
+            handle: `user`,
+            createdAt: `2020-07-07T03:07:42.389Z`,
+            imageURL: `image/adhiqhidqd/dqdqfq`,
+            bio: `Hello, my name is user, nice to meet you`,
+            website: `https://user.com`,
+            location: `Cebu, PH`
+        }
+    ],
+    
     screams: [
         {
-            userHandle: "user",
-            body: "this is a scream body",
-            createdAt: "2020-07-07T03:07:42.389Z",
+            userHandle: `user`,
+            body: `this is a scream body`,
+            createdAt: `2020-07-07T03:07:42.389Z`,
             likeCount: 5,
             commentCount:2
         }
     ]
 };
+
+const userDetails = {
+    //Redux data
+    credentials: {
+        userId: `dasdjojh13123123`,
+        email: `user@email.com`,
+        handle: `user`,
+        createdAt: `2020-07-07T03:07:42.389Z`,
+        imageURL: `image/adhiqhidqd/dqdqfq`,
+        bio: `Hello, my name is user, nice to meet you`,
+        website: `https://user.com`,
+        location: `Cebu, PH`
+    },
+    likes: [
+        {
+            userhandle: `user`,
+            screamId: `fji14254hi42n4n2n4`
+        },
+        {
+            userhandle: `user`,
+            screamId: `fifnnn5jn35jnn5jkbb`
+        }
+    ]
+}
--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -6,9 +6,10 @@ const config = require(`../util/config`);
 const firebase = require('firebase');
 firebase.initializeApp(config)
 
-const { validateSignupData, validateLoginData } = require(`../util/validators`);
+const { validateSignupData, validateLoginData, reduceUserDetails } = require(`../util/validators`);
 const { fileURLToPath } = require('url');
 
+// Sign up new users
 exports.signup = (req, res) => {
     const newUser = {
         email: req.body.email,
@@ -63,6 +64,7 @@ exports.signup = (req, res) => {
         });
 }
 
+// Log user in
 exports.login = (req, res) => {
     const user = {
         email: req.body.email,
@@ -93,6 +95,44 @@ exports.login = (req, res) => {
         });
 }
 
+// Add user details
+exports.addUserDetails = (req, res) => {
+    let userDetails = reduceUserDetails(req.body);
+
+    db.doc(`/users/${req.user.handle}`).update(userDetails)
+        .then(() => {
+            return res.json({ message: `Details added successfuly`})
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({error: err.code});
+        });
+};
+
+// Get own user details
+exports.getAuthenticatedUser = (req, res) => {
+    let userData = {};
+    db.doc(`/users/${req.user.handle}`).get()
+        .then(doc => {
+            if(doc.exists){
+                userData.credentials = doc.data();
+                return db.collection(`likes`).where(`userHandle`, `==`, req.user.handle).get()
+            }
+        })
+        .then(data => {
+            userData.likes = [];
+            data.forEach(doc => {
+                userData.likes.push(doc.data());
+            });
+            return res.json(userData);
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: err.code});
+        });
+    
+};
+// Upload a profile image
 exports.uploadImage = (req, res) => {
     const BusBoy = require(`busboy`);
     const path = require(`path`);
